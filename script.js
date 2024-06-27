cdc9ea4e86939503bff27a8b8e2c0baa5dedb7fe
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-06-27 10:37:23 -0300

    Create functions for the 4 main math operations.
    - add,subtract,multiply,divide.
    - declare operator, firstNumber, secondNumber variables.

---
+++ script.js
@@ -0,0 +1,28 @@
+//FUNCTIONS
+
+function add(num1,num2){
+    return num1+num2;
+}
+
+function subtract(num1,num2){
+    return num1-num2;
+}
+
+function multiply(num1,num2){
+    return num1*num2;
+}
+
+function divide(num1,num2){
+    return num1/num2;
+}
+
+function operate(operator,firstNumber,secondNumber){
+    add(firstNumber,secondNumber)
+}
+
+//VARIABLES
+
+let firstNumber,secondNumber,operator;
+
+
+//DOM MANIPULATION
