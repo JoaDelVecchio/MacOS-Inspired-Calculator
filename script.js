85d26a5e9460da9aafaf69c86461e9a25abc0fa1
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-07-01 10:30:10 -0300

    Update operate function
    - Now it calls the other functions depending on the operator.

--- script.js
+++ script.js
@@ -16,13 +16,27 @@ function divide(num1,num2){
     return num1/num2;
 }
 
-function operate(operator,firstNumber,secondNumber){
-    add(firstNumber,secondNumber)
+function operate(operator,num1,num2){
+    switch(operator){
+        case "+":
+            return add(num1,num2)
+        case "-":
+            return subtract(num1,num2);
+        case "*":
+            return multiply(num1,num2);
+        case "/":
+            return divide(num1,num2);
+        default:
+            return null;
+    }
 }
 
 //VARIABLES
 
-let firstNumber,secondNumber,operator;
+let num1=0,num2,operator = null,displayValue,opsButtons;
 
 
 //DOM MANIPULATION
+
+
+
